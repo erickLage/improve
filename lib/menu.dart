d407fed3bfa48c7a243407fcbb5414d68e3ddf2c
Lucca Soares de P Lacerda <luccasplacerda@gmail.com>
2020-11-03 20:30:19 -0300

    Tela ajuda, inicio

--- lib/menu.dart
+++ lib/menu.dart
@@ -8,80 +8,74 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                height: 100,
-                width: MediaQuery.of(context).size.width,
-                child: Image.asset(
-                  'src/assets/logo_sem_fundo.png'
+        body: SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 100,
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset('src/assets/logo_sem_fundo.png'),
+            ),
+            SizedBox(
+              height: 80,
+            ),
+            Visibility(
+              visible: user != null && user.getID() != '-1',
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Bem vindo, " + user.getName(),
+                  style: TextStyle(fontSize: 18, color: Colors.blue),
                 ),
               ),
-              SizedBox(
-                height: 80,
-              ),
-              Visibility(
-                visible: user != null && user.getID() != '-1',
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text("Bem vindo, "+user.getName(), style: TextStyle(fontSize: 18, color: Colors.blue),),
-                ),
-              ),
-              RaisedButton(
-                color: Colors.blue,
-                onPressed: ()async{
-                  await Navigator.pushNamed(context, '/login');
-                },
-                child: Container(
+            ),
+            RaisedButton(
+              color: Colors.blue,
+              onPressed: () async {
+                await Navigator.pushNamed(context, '/login');
+              },
+              child: Container(
                   alignment: Alignment.center,
                   width: MediaQuery.of(context).size.width,
-                  child: Text(user == null || user.getID() == '-1' ? 'Entrar' : 'Alterar conta')
-                ),
-              ),  
-              RaisedButton(
-                color: Colors.lightGreen[400],
-                onPressed: (){
-                  
-                },
-                child: Container(
+                  child: Text(user == null || user.getID() == '-1'
+                      ? 'Entrar'
+                      : 'Alterar conta')),
+            ),
+            RaisedButton(
+              color: Colors.lightGreen[400],
+              onPressed: () {},
+              child: Container(
                   alignment: Alignment.center,
                   width: MediaQuery.of(context).size.width,
-                  child: Text('Jogar')
-                ),
-              ),
-              RaisedButton(
-                color: Colors.cyan,
-                onPressed: (){
-                },
-                child: Container(
+                  child: Text('Jogar')),
+            ),
+            RaisedButton(
+              color: Colors.cyan,
+              onPressed: () {},
+              child: Container(
                   alignment: Alignment.center,
                   width: MediaQuery.of(context).size.width,
-                  child: Text('Ver pontuação')
-                ),
-              ),
-              RaisedButton(
-                color: Colors.pink[300],
-                onPressed: (){
-                  
-                },
-                child: Container(
+                  child: Text('Ver pontuação')),
+            ),
+            RaisedButton(
+              color: Colors.pink[300],
+              onPressed: () {
+                Navigator.pushNamed(context, "/ajuda");
+              },
+              child: Container(
                   alignment: Alignment.center,
                   width: MediaQuery.of(context).size.width,
-                  child: Text('Personalizar')
-                ),
-              ),
-            ],
-          ),
+                  child: Text('Personalizar')),
+            ),
+          ],
         ),
-      )
-    );
+      ),
+    ));
   }
 }
