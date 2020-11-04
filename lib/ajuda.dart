2bff840eb13e38a5fda2f95fa4fa9779267d7fec
Rebeca Neto <rebecabsneto@gmail.com>
2020-11-03 21:26:16 -0300

    Continuar ajuda

--- lib/ajuda.dart
+++ lib/ajuda.dart
@@ -8,8 +8,9 @@ class Ajuda extends StatefulWidget {
 }
 
 class _AjudaState extends State<Ajuda> {
-  PageController pageController = new PageController(initialPage: 0, viewportFraction: 1);
-  int paginaAtual = 0; 
+  PageController pageController =
+      new PageController(initialPage: 0, viewportFraction: 1);
+  int paginaAtual = 0;
   List<String> titulo = [
     'Bem vindo ao Improve!',
     'Cadastre-se',
@@ -28,7 +29,7 @@ class _AjudaState extends State<Ajuda> {
     Image.asset('src/assets/logo_sem_fundo.png', color: Colors.white),
     Icon(Icons.face, size: 100, color: Colors.white),
     Icon(Icons.create, size: 100, color: Colors.white),
-    Icon(Icons.games, size: 100, color: Colors.white),
+    Icon(Icons.sports_esports, size: 100, color: Colors.white),
     Icon(Icons.share, size: 100, color: Colors.white)
   ];
 
@@ -37,12 +38,12 @@ class _AjudaState extends State<Ajuda> {
     return Scaffold(
       body: SafeArea(
           child: Column(
-        children: [ 
+        children: [
           Expanded(
             child: PageView.builder(
               controller: pageController,
               itemCount: 5,
-              onPageChanged: (index){
+              onPageChanged: (index) {
                 setState(() {
                   paginaAtual = index;
                 });
@@ -51,31 +52,26 @@ class _AjudaState extends State<Ajuda> {
                 return Container(
                   color: randomColor(),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      icones[index],
-                      Text(
-                        titulo[index],
-                        style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white
-                        ),
-                      ),
-                      SizedBox(height: 5),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 30.0),
-                        child: Text(
-                          descricao[index],
-                          textAlign: TextAlign.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        icones[index],
+                        Text(
+                          titulo[index],
                           style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white
-                          ),
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
                         ),
-                      )
-                    ]
-                  ),
+                        SizedBox(height: 5),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 30.0),
+                          child: Text(
+                            descricao[index],
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 16, color: Colors.white),
+                          ),
+                        )
+                      ]),
                 );
               },
             ),
@@ -89,7 +85,8 @@ class _AjudaState extends State<Ajuda> {
                   child: FlatButton(
                     child: Text("Pular"),
                     onPressed: () {
-                      Navigator.pushNamedAndRemoveUntil(context, '/menu', (route) => false);
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, '/menu', (route) => false);
                     },
                   ),
                 ),
@@ -102,12 +99,13 @@ class _AjudaState extends State<Ajuda> {
                       return Container(
                         width: 8.0,
                         height: 8.0,
-                        margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 2.0),
+                        margin: EdgeInsets.symmetric(
+                            vertical: 5.0, horizontal: 2.0),
                         decoration: BoxDecoration(
                           shape: BoxShape.circle,
                           color: paginaAtual == index
-                            ? Color.fromRGBO(0, 0, 0, 0.9)
-                            : Color.fromRGBO(0, 0, 0, 0.4),
+                              ? Color.fromRGBO(0, 0, 0, 0.9)
+                              : Color.fromRGBO(0, 0, 0, 0.4),
                         ),
                       );
                     }).toList(),
@@ -130,7 +128,7 @@ class _AjudaState extends State<Ajuda> {
               ],
             ),
           )
-        ], 
+        ],
       )),
     );
   }
