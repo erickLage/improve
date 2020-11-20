075d51c8e3d9a99d4f34b75b6158f02249abe530
erickLage <erick-lage@hotmail.com>
2020-11-20 10:53:14 -0300

    IMplementar tela de pontuacao

--- lib/pontuacao.dart
+++ lib/pontuacao.dart
@@ -6,6 +6,17 @@ class Pontuacao extends StatefulWidget {
 }
 
 class _PontuacaoState extends State<Pontuacao> {
+
+  User user;
+  List<Map> pontuacoes; 
+
+  @override
+  void didChangeDependencies() {
+    user = ModalRoute.of(context).settings.arguments;
+    pontuacoes = user.getPontuacoes();
+    pontuacoes.sort((a, b) => b['pontuacao'] - a['pontuacao'] );
+    super.didChangeDependencies();
+  }
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -31,7 +42,35 @@ class _PontuacaoState extends State<Pontuacao> {
   }
 
   Widget jogoImagens(){
-    return Container(color: Colors.red);
+    return Container(
+      child: ListView.builder(
+        itemCount: pontuacoes.length,
+        physics: BouncingScrollPhysics(),
+        itemBuilder: (context, index){
+          return Card(
+            elevation: 2,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              height: 50,
+              child: Row(
+                children: [
+                  Text((index+1).toString() + 'º', style: TextStyle(fontSize: 22)),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 5),
+                    child: Container(
+                      color: Colors.black,
+                      width: 1,
+                    ),
+                  ),
+                  Text(pontuacoes[index]['pontuacao'].toString(), style: TextStyle(fontSize: 18)),
+                  Expanded(child: SizedBox()),
+                ],
+              ),
+            ),
+          );
+        }
+      ),
+    );
   }
 
   Widget jogo1(){
